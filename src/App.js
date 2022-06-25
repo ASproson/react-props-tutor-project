fbe4454f403a6097768d9bb5f1c5da8d72da3ab0
atlas <a.sproson@outlook.com>
2022-06-25 19:00:16 +0100

    Adds props passing capability and state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import "./App.css";
+import CollectProps from "./components/CollectProps";
+import React, { useState } from "react";
 
 function App() {
+  const data = [2, 4, 6, 8];
+  const [person, setPerson] = useState({
+    name: "John",
+    age: "20",
+  });
+
+  const personSetter = (person) => {
+    setPerson(person);
+  };
+
   return (
     <div className="App">
       <h1>React State Management</h1>
+      <CollectProps data={data} personSetter={personSetter} />
+      <h2>Name: {person.name}</h2>
+      <h2>Age: {person.age}</h2>
     </div>
   );
 }
